f171d9186a6fb92e94ca26b36bc274a618d7e169
Meysam NajafiTavani <meysamnajafi67@yahoo.com>
2019-12-19 17:45:37 -0800

    save file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const util = require("util");
+const fs = require("fs");
+ 
 
 const app = express();
 
@@ -10,10 +11,50 @@ var PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+ 
+
+
+app.get("/notes", function (req, res) {
+
+notes
+        .getnotes()
+        .then(notes => res.json(notes))
+
+        .catch(err => res.status(500).json(err));
+});
+
+
+app.post("/notes", function (req, res) {
+
+    notes
+        .postnotes()
+        .then(notes => res.json(notes))
+
+        .catch(err => res.status(500).json(err));
+});
+app.delete("/notes/:id", function (req, res) {
+    store
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err));
+});
+app.put("/notes/:id", function (req, res) {
+    store
+        .updateNOte(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err));
+});
+
 
 
 //call the server
 
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+app.listen(PORT, () => console.log("Listening on PORT:" + PORT ));
